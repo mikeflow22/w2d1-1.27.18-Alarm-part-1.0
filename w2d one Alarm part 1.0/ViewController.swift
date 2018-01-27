2d9e8d49fb4b996b21d0e1fd73ab711637fa9df5
Michael R.F <mrflowers22@uwalumni.com>
2018-01-27 14:25:06 -0700

    Initial Commit

---
+++ w2d one Alarm part 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  w2d one Alarm part 1.0
+//
+//  Created by Michael Flowers on 1/27/18.
+//  Copyright © 2018 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
